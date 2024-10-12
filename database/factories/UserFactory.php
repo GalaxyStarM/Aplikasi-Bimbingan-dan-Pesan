e9f9e5747a02ff248d207cf196d334682f62c41c
SyahirahTriMeilina25 <syahirah.tri0255@student.unri.ac.id>
2024-10-12 21:57:23 +0700

    menambahkan halaman login

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,33 +2,31 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected $model = User::class;
 
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password123'), // Default password
+            'role' => $this->faker->randomElement(['mahasiswa', 'dosen']), // Role random
+            'nip' => $this->faker->numberBetween(100000000000000000, 999999999999999999), // Contoh NIP
+            'nim' => $this->faker->numberBetween(2021000000, 2021999999), // Contoh NIM
         ];
     }
 
