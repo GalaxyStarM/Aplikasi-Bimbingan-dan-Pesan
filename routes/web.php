d579a31de8efa578dc32d918f63f8ed8a3ee6ac3
maya2712 <desimaya2712sari@gmail.com>
2024-10-13 14:17:42 +0700

    Desi menambahkan halaman isi pesan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/isipesan', function () {
+    return view('isipesan'); // Pastikan ini merujuk ke file isipesan.blade.php
 });
