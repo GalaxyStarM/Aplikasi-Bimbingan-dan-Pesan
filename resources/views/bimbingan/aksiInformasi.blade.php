c9d69a2f56dfecaf0f1db3437157059f2e15f233
Tri Murniati <tri.murniati2735@student.unri.ac.id>
2024-10-21 21:02:45 +0700

    mengubah tebal font di setiap laman

--- resources/views/bimbingan/aksiInformasi.blade.php
+++ resources/views/bimbingan/aksiInformasi.blade.php
@@ -7,8 +7,7 @@
     <title>SITEI | Detail Mahasiswa</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Viga&display=swap" rel="stylesheet">
 
     <style>
         /* Yang diambil */
